a9366f184b638902704c7a838b81544c7c81cf1e
Fabio Malizia <fmalizia@gmail.com>
2019-04-06 21:02:27 +0200

    Esercizio completo

---
+++ src/app/multiple-form-controls/multiple-form-controls.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MultipleFormControlsComponent } from './multiple-form-controls.component';
+
+describe('MultipleFormControlsComponent', () => {
+  let component: MultipleFormControlsComponent;
+  let fixture: ComponentFixture<MultipleFormControlsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MultipleFormControlsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MultipleFormControlsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
